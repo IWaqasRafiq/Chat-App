4f8f04915787df89ee28a60683deac031348a5eb
Muhammad Waqas <119754074+IWaqasRafiq@users.noreply.github.com>
2025-08-24 23:05:02 +0500

    onlick function

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -11,6 +11,7 @@ const SideBar = () => {
 
   const { logout, onlineUsers } = useContext(AuthContext);
   const [input, setInput] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const navigate = useNavigate();
 
@@ -33,8 +34,9 @@ const SideBar = () => {
               src={assets.menu_icon}
               alt="Menu"
               className="max-h-5 cursor-pointer"
+              onClick={() => setOpen(!open)}
             />
-            <div className="absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block ">
+            <div className={`absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 ${open ? "block" : "hidden"}`}>
               <p
                 onClick={() => navigate("/profile")}
                 className="cursor-pointer text-sm"
